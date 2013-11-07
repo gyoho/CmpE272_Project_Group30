374741374c9ea794aac6c912bc1328b1cd5a8d4d
gyoho <gyoho@hotmail.co.jp>
2013-11-07 15:40:33 -0800

    Bootstrap

---
+++ public/javascripts/index.js
@@ -0,0 +1,34 @@
+function drawImage(imageObj) {
+  var stage = new Kinetic.Stage({
+    container: "container",
+    width: 578,
+    height: 200
+  });
+  var layer = new Kinetic.Layer();
+
+  // darth vader
+  var darthVaderImg = new Kinetic.Image({
+    image: imageObj,
+    x: stage.getWidth() / 2 - 200 / 2,
+    y: stage.getHeight() / 2 - 137 / 2,
+    width: 200,
+    height: 137,
+    draggable: true
+  });
+
+  // add cursor styling
+  darthVaderImg.on('mouseover', function() {
+    document.body.style.cursor = 'pointer';
+  });
+  darthVaderImg.on('mouseout', function() {
+    document.body.style.cursor = 'default';
+  });
+
+  layer.add(darthVaderImg);
+  stage.add(layer);
+}
+var imageObj = new Image();
+imageObj.onload = function() {
+  drawImage(this);
+};
+imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';
