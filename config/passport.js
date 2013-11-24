c7cac4a6e21fc40e760faca9c2afe04401567bf8
gyoho <gyoho@hotmail.co.jp>
2013-11-24 09:10:28 -0800

    Needs to fix login

---
+++ config/passport.js
@@ -0,0 +1,31 @@
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy
+  , User = require('../models/user');
+
+	passport.use(new LocalStrategy({
+    email: 'email',
+    password: 'password'
+  },
+  function(email, password, done) {
+    User.findOne({ email: email }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
